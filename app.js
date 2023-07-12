7c922a70a04c7c840278d3e793a7f9b695bca01f
BrinDha1A <139069052+BrinDha1A@users.noreply.github.com>
2023-07-12 11:10:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var image=document.getElementById("bulb");
+
+function bulb_on()
+{
+    image.src="./images/ON.jpg"
+}
+
+function bulb_off()
+{
+    image.src="./images/OFF.jpg"
+}
